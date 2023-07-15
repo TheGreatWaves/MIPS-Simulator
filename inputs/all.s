abb30f7700355000bdf8b4a7f4fc9597996a12ae
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-07-15 21:23:45 +0700

    updated all.s test

--- inputs/all.s
+++ inputs/all.s
@@ -1,12 +1,11 @@
-# J JAL BEQ BNE BLEZ BGTZ
+# BLEZ BGTZ
 # SLTI SLTIU ANDI ORI
 # XORI LUI LB LH LW LBU
 # LHU SB SH SW BLTZ BGEZ
 # BLTZAL BGEZAL SLL SRL SRA SLLV
-# SRLV SRAV JR JALR ADD ADDU
-# NOR
-# SLT SLTU MULT MFHI MFLO MTHI
-# MTLO MULTU DIV DIVU SYSCALL
+# SRLV SRAV JALR 
+# SLT SLTU    
+# DIV DIVU 
 
 # This test assumes that all jump and branching instruction works.
 
@@ -20,6 +19,8 @@ reset:
         addi $t2, $zero, 0
         addi $t3, $zero, 0
         addi $t4, $zero, 0
+        mthi $zero
+        mtlo $zero
         jr $ra
 start_test:
 test_addi_pos:
@@ -31,6 +32,34 @@ test_addi_pos:
 test_addi_neg:
         # test ADDI negative
         addi $t0, $t0, -1
+        lui $t1, 0xFFFF
+        ori $t1, $t1, 0xFFFF
+        bne $t0, $t1, inf
+        jal reset
+test_add_pos:
+        # test add positive
+        ori $t1, $t1, 0x1a
+        add $t0, $t0, $t1
+        bne $t0, $t1, inf
+        jal reset
+test_add_neg:
+        # test add negative
+        addi $t2, $t2, -1
+        add $t0, $t0, $t2
+        lui $t1, 0xFFFF
+        ori $t1, $t1, 0xFFFF
+        bne $t0, $t1, inf
+        jal reset
+test_addu_pos:
+        # test addu positive
+        ori $t1, $t1, 0x1a
+        addu $t0, $t0, $t1
+        bne $t0, $t1, inf
+        jal reset
+test_addu_neg:
+        # test addu negative
+        addi $t2, $t2, -1
+        addu $t0, $t0, $t2
         lui $t1, 0xFFFF
         ori $t1, $t1, 0xFFFF
         bne $t0, $t1, inf
@@ -197,6 +226,53 @@ test_nor_10:
         addi $t3, $zero, -2
         nor $t2, $t0, $t1
         bne $t2, $t3, inf
+        jal reset
+test_mthi_mfhi:
+        addi $t0, $zero, 0xbeef
+        mthi $t0
+        mfhi $t2
+        bne $t0, $t2, inf
+        mthi $zero
+        jal reset
+test_mtlo_mflo:
+        addi $t0, $zero, 0xdead
+        mtlo $t0
+        mflo $t2
+        bne $t0, $t2, inf
+        mtlo $zero
+        jal reset
+test_mult_pos:
+        addi $t0, $t0, 333        
+        addi $t1, $t1, 111
+        mult $t1, $t0
+        mfhi $t2
+        mflo $t3
+        ori $t4, $t4, 0x9063
+        bne $t2, $zero, inf
+        bne $t3, $t4, inf
+        jal reset
+test_mult_neg:
+        addi $t0, $t0, 333        
+        addi $t1, $t1, -111
+        mult $t1, $t0
+        mfhi $t2
+        mflo $t3
+        ori $t1, $zero, -1
+        ori $t4, $t4, -36963
+        bne $t2, $t1, inf
+        bne $t3, $t4, inf
+        jal reset
+test_multu_pos:
+        addi $t0, $t0, 333        
+        addi $t1, $t1, -111
+        multu $t1, $t0
+        mfhi $t2
+        mflo $t3
+        ori $t1, $zero, 0x14c # testing hi
+        bne $t2, $t1, inf
+        lui $t4, 0xffff       # testing lo
+        ori $t4, $t4, 0x6f9d
+        bne $t3, $t4, inf
         jal reset
 done:
         j exit
