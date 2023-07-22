757aacff79e670670bb91376388b467b76171c5f
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-07-22 19:30:41 +0700

    splitting tests

---
+++ tests/test_addi.s
@@ -0,0 +1,29 @@
+.text
+
+main:   
+        j start_test
+reset:
+        addi $t0, $zero, 0
+        addi $t1, $zero, 0
+        addi $t2, $zero, 0
+        addi $t3, $zero, 0
+        addi $t4, $zero, 0
+        addi $t6, $t6, 1
+        mthi $zero
+        mtlo $zero
+        jr $ra
+start_test:
+test_addi_pos:
+                                      # test ADDI positive
+        addi $t0, $t0, 0x1a
+        ori $t1, $t1, 0x1a
+        bne $t0, $t1, inf
+        jal reset
+done:
+        j exit
+inf:
+        addi $t7, $t7, -1
+        j exit
+exit:
+        addiu $v0, $zero, 0xa
+        syscall
