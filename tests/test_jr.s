849e20e4229644b246f719b98157bba4cb8a8a51
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-07-24 07:23:45 +0700

    finalized

--- tests/test_jr.s
+++ tests/test_jr.s
@@ -8,10 +8,10 @@
 main:   
         lui $t3, 0x0040        # 0x00
         ori $t3, $t3, 0x18     # 0x04
-        addiu $t0, $zero, 255  # 0x08
+        addi $t0, $zero, 255  # 0x08
         jr exit                # 0x0c 
-        addiu $t0, $zero, 5    # 0x10
-        addiu $t1, $zero, 10   # 0x14
+        addi $t0, $zero, 5    # 0x10
+        addi $t1, $zero, 10   # 0x14
 exit:
-        addiu $v0, $zero, 0xa  # 0x18
+        addi $v0, $zero, 0xa  # 0x18
         syscall
