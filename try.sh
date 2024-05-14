f7dba11f84eee28c3f37aa82f83aaf1248d9d886
Onami <106456906+TheGreatWaves@users.noreply.github.com>
2024-05-14 21:31:05 +0700

    chore: cleaned up run script

--- try.sh
+++ try.sh
@@ -2,10 +2,6 @@
 inpt_name="$1"
 new_ext=".x"
 opt_name="${inpt_name/.s/"$new_ext"}"    
-# prints 'I love Sara and Marry'
-
-# hexdump
-
 cd spim_assembler
 python3 ./asm2hex "../tests/$1"
 cd ../src
